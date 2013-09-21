e93b447b06a9853c05b742b01787847c931dcef4
Lars Wuckel <lars.wuckel@gmail.com>
2013-09-21 22:05:19 +0200

    nur die Liste

--- source/App.js
+++ source/App.js
@@ -1,26 +1,7 @@
 enyo.kind({
 	name: "App",
-	kind: "FittableRows",
 	fit: true,
-	components:[{
-		kind: "Panels"
-		, classes:"enyo-fit"
-        , arrangerKind: "CollapsingArranger"
-        , components: [
-            {kind:"Panels", name:"listPanels"
-             , classes:"onyx todos-panels"
-             , arrangerKind:"CarouselArranger"
-             , components: [
-                {kind:"FittableRows"
-                 , components: [
-					{kind: "onyx.Toolbar"
-					, layoutKind:"FittableColumnsLayout"
-                     , components: [{content:"Aufgaben", fit:true},
-					{name: "list", kind: "TaskList"}]
-				}
-				]
-			}]
-		}
-		]}
+	components:[
+	{name: "list", kind: "TaskList"}
 	]
 });
